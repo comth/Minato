443b20f1396fbed54fd4edc24a346cf1df5f0dd0
Guilherme Carvalho <ragnaphanton@gmail.com>
2020-09-16 17:30:34 -0300

    Configuração e modelagem inicial

---
+++ Minato/Models/Endereco.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Minato.Models
+{
+    public class Endereco
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O Bairro é obrigatório.")]
+        [StringLength(60, ErrorMessage = "O Bairro deve ter no máximo 60 caracteres.")]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "O CEP é obrigatório")]
+        [StringLength(8, ErrorMessage = "O CEP deve ter 8 caracteres.")]
+        public string Cep { get; set; }
+
+        [Required(ErrorMessage = "O Logradouro é obrigatório.")]
+        [StringLength(80, ErrorMessage = "O Logradouro deve ter no máximo 80 caracteres.")]
+        public string Logradouro { get; set; }
+
+        [Required(ErrorMessage = "O Complemento é obrigatório.")]
+        [StringLength(60, ErrorMessage = "O Complemento deve ter no máximo 60 caracteres.")]
+        public string Complemento { get; set; }
+
+        [StringLength(80, ErrorMessage = "A Observação deve ter no máximo 80 caracteres.")]
+        public string Observacao { get; set; }
+
+        [Required(ErrorMessage = "O UF é obrigatório.")]
+        [StringLength(2, ErrorMessage = "O UF deve ter no máximo 2 caracteres.")]
+        public string Uf { get; set; }
+    }
+}
---
+++ Minato/Models/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Minato.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O Nome é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O Nome deve ter no máximo 100 caracteres.")]
+        public string Nome { get; set; }
+
+        public List<Endereco> Enderecos { get; set; }
+
+        public List<Telefone> Telefones { get; set; }
+    }
+
+    public class Telefone
+    {
+        [Required(ErrorMessage = "O Telefone é obrigatório.")]
+        [StringLength(11, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 10)]
+        public string TelefoneString { get; set; } //trocar
+    }
+}
