da8053211c8c5ea91fd9e9e41f9ea88f6b4f5b1e
Guilherme Carvalho <ragnaphanton@gmail.com>
2020-10-01 18:06:23 -0300

    alteração menu

--- Minato/ClientApp/src/app/home/home.component.ts
+++ Minato/ClientApp/src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { ProdutoService } from '../services/produto.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
 
   constructor(private produtoService: ProdutoService) {
