6102597e11ba2425859ebb695dfb713fe9206c08
comth <taahnoidea@gmail.com>
2020-11-17 15:12:12 -0300

    Atualização do Net Core

---
+++ Minato/Migrations/20201117175448_Mesas.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Minato.Migrations
+{
+    public partial class Mesas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pedido_Endereco_EnderecoSelecionadoId",
+                table: "Pedido");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pedido_Usuario_UsuarioId",
+                table: "Pedido");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UsuarioId",
+                table: "Pedido",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EnderecoSelecionadoId",
+                table: "Pedido",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateTable(
+                name: "Status",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Status", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mesa",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Numero = table.Column<int>(type: "int", nullable: false),
+                    PedidoId = table.Column<int>(type: "int", nullable: true),
+                    PedidoDataPedido = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    StatusId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mesa", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Mesa_Pedido_PedidoId_PedidoDataPedido",
+                        columns: x => new { x.PedidoId, x.PedidoDataPedido },
+                        principalTable: "Pedido",
+                        principalColumns: new[] { "Id", "DataPedido" },
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Mesa_Status_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "Status",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mesa_PedidoId_PedidoDataPedido",
+                table: "Mesa",
+                columns: new[] { "PedidoId", "PedidoDataPedido" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mesa_StatusId",
+                table: "Mesa",
+                column: "StatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pedido_Endereco_EnderecoSelecionadoId",
+                table: "Pedido",
+                column: "EnderecoSelecionadoId",
+                principalTable: "Endereco",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pedido_Usuario_UsuarioId",
+                table: "Pedido",
+                column: "UsuarioId",
+                principalTable: "Usuario",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pedido_Endereco_EnderecoSelecionadoId",
+                table: "Pedido");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pedido_Usuario_UsuarioId",
+                table: "Pedido");
+
+            migrationBuilder.DropTable(
+                name: "Mesa");
+
+            migrationBuilder.DropTable(
+                name: "Status");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UsuarioId",
+                table: "Pedido",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EnderecoSelecionadoId",
+                table: "Pedido",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pedido_Endereco_EnderecoSelecionadoId",
+                table: "Pedido",
+                column: "EnderecoSelecionadoId",
+                principalTable: "Endereco",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pedido_Usuario_UsuarioId",
+                table: "Pedido",
+                column: "UsuarioId",
+                principalTable: "Usuario",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
