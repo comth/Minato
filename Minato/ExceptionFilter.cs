3e7a9a61171488f651c0cbf89a7ca2e78a421f3c
comth <taahnoidea@gmail.com>
2021-04-13 17:01:49 -0300

    update material and core-js

--- Minato/ExceptionFilter.cs
+++ Minato/ExceptionFilter.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Minato
 {
-    public class ExceptionFilter
+    public class ExceptionFilter : ExceptionFilterAttribute, IExceptionFilter
     {
+        public override void OnException(ExceptionContext context)
+        {
+            var error = new 
+            {
+                StatusCode = 500,
+                Message = "Something went wrong! Internal Server Error."
+            };
+
+            context.Result = new JsonResult(error);
+        }
     }
 }
